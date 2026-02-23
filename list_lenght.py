773f19585618e621f55c9c2ab3314b2af38ed9a7
Alby-3 <albymanoj525@gmail.com>
2026-02-23 21:17:44 +0530

    Add files via upload

---
+++ list_lenght.py
@@ -0,0 +1,11 @@
+"""Write a Python program:
+Create list:
+data = [12, 45, 67, 89, 23, 56]
+Print:
+Total elements: 6
+Rules
+se:
+len()
+Output format must match exactly."""""
+data=[12, 45, 67, 89, 23, 56]
+print("Total element =" ,len(data))
