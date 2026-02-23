80f1157badc9f1855b26c69bcf980b1f99d70788
Alby-3 <albymanoj525@gmail.com>
2026-02-23 21:21:04 +0530

    Add files via upload

---
+++ list_append.py
@@ -0,0 +1,16 @@
+"""Write program:
+Create empty list:
+items = []
+Add:
+10
+20
+30
+using append()
+Print final list
+Output must be:
+[10, 20, 30]"""
+items=[]
+items.append(10)
+items.append(20)
+items.append(30)
+print(items)
